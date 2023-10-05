ee5c8f7e74fde5299bc8840dcf7d036984751693
finn-chipp <67271888+finn-chipp@users.noreply.github.com>
2023-10-05 10:54:57 +0100

    Update conkit.h
    Add GPL brief to top of header

--- conkit.h
+++ conkit.h
@@ -1,3 +1,20 @@
+/* Conkit -- cross-platform console-operation abstraction header
+ * Copyright (C) 2023 Finn Chipp
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h> // For i/o
 #include <stdlib.h> // For calloc(), free(), and size_t
 #include <math.h> // For number of chars that size_t is to print
